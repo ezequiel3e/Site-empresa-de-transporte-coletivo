42a387f0313611a3823ef5e2929b6eeab68a043f
ezequiel3e <esequieldani33@gamil.com>
2023-06-25 13:00:50 -0300

    Site Transporte coletivo

---
+++ script.js
@@ -0,0 +1,37 @@
+var ul = document.querySelector('nav ul');
+var menuBtn = document.querySelector('.menu-btn i');
+
+function menuShow() {
+    if (ul.classList.contains('open')) {
+        ul.classList.remove('open');
+    }else{
+        ul.classList.add('open');
+    }
+}
+
+
+
+
+
+
+
+
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
